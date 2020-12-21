ca523d00e267871ba45b5d74914f424a7b5b25bf
Snehalata Sahu <sneha16sahu@gmail.com>
2020-12-21 19:08:20 +0530

    How it works page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 import Home from "./Pages/Home";
-import './App.css'
+import "./App.css";
+import { Container } from "react-bootstrap";
+import HowItWorks from "./Pages/HowItWorks";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <Home/>
+    <Router>
+      
+      <Switch>
+      <Route exact path="/" component={Home} />
+        <Route exact path="/howitworks" component={HowItWorks} />
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/Pages/HowItWorks.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from "react";
+
+import Header from "../Components/Home/Header";
+import ServicesSection from "../Components/Home/ServicesSection";
+import StrategySection from "../Components/Home/StrategySection";
+import NavBar from "../Components/Home/Navbar";
+import Footer from "../Components/Home/Footer";
+import Contact from "../Components/Home/Contact";
+import Carasoul from "../Components/Home/Carasoul";
+import Cards from "../Components/Home/Cards";
+import SecNavbar from "../Components/HowItWorks/SecNavbar";
+
+const HowItWorks = () => {
+  return (
+    <Fragment>
+      <NavBar />
+      <SecNavbar />
+      <Header />
+      <StrategySection />
+      <ServicesSection />
+      <Carasoul />
+      <Cards />
+      <Contact />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default HowItWorks;
